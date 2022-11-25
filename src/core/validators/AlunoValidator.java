e3bd58f1b0a8fe5a825e6465d57a0591aa5931ec
RafaCompri <rafa.compri@outlook.com>
2022-11-25 20:11:28 -0300

    Troca da pasta SRC

---
+++ src/core/validators/AlunoValidator.java
@@ -0,0 +1,40 @@
+package core.validators;
+
+import app.utils.CPFValidator;
+import core.entities.Aluno;
+import core.interfaces.IValidator;
+
+public class AlunoValidator implements IValidator<Aluno>{
+	public boolean validate(Aluno obj) {
+		EnderecoValidator enderecoValidator = new EnderecoValidator();
+
+		if (obj.getFiliado().getCpf() == null || obj.getFiliado().getCpf().equals(""))
+			return false;
+
+		if (obj.getFiliado().getNome() == null || obj.getFiliado().getNome().equals(""))
+			return false;
+
+		if (obj.getFiliado().getEmail() == null || obj.getFiliado().getEmail().equals(""))
+			return false;
+
+		if (obj.getFiliado().getTelefone1() == null || obj.getFiliado().getTelefone1().equals(""))
+			return false;
+
+		if (obj.getFiliado().getRg() == null)
+			return false;
+
+		if (obj.getFiliado().getRg().getNumero() == null || obj.getFiliado().getRg().getNumero().equals(""))
+			return false;
+
+		if (obj.getFiliado().getRg().getOrgaoExpedidor() == null || obj.getFiliado().getRg().getOrgaoExpedidor().equals(""))
+			return false;
+
+		if (!CPFValidator.isCPF(obj.getFiliado().getCpf()))
+			return false;
+
+		if (!enderecoValidator.validate(obj.getFiliado().getEndereco()))
+			return false;
+
+		return true;
+	}
+}
